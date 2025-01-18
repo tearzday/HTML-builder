b02931e83d7795cf98626eef7eab7747cbe54ba8
Daniil Biver <d.biver@sfbtech.org>
2025-01-18 14:55:23 +0300

    fix: refactor 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,7 +5,4 @@ const { stdout } = process;
 const pathToTxtFile = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(pathToTxtFile, 'utf-8');
 
-let textData = '';
-
-stream.on('data', (chunk) => (textData += chunk));
-stream.on('end', () => stdout.write(textData));
+stream.on('data', (data) => stdout.write(data));
