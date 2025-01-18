7bc6fcc268af0be357d30646b030ca0d1297f116
Daniil Biver <d.biver@sfbtech.org>
2025-01-18 14:31:47 +0300

    feat: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const { stdin, stdout } = process;
+const readline = require('node:readline');
+const fs = require('fs');
+const path = require('path');
+
+const pathToTxtFile = path.join(__dirname, 'text.txt');
+const stream = fs.createWriteStream(pathToTxtFile);
+
+stdout.write('Specify the text to be added to the file\n');
+
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+rl.on('line', (data) => {
+  if (data === 'exit') {
+    closeReadline();
+  } else {
+    stream.write(data + '\n');
+  }
+});
+
+rl.on('SIGINT', () => {
+  closeReadline();
+});
+
+function closeReadline() {
+  rl.close();
+  stdout.write('Goodbay\n');
+}
